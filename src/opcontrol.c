51aaa1e0fec920b4b449cd6a1c63d002eb142d12
kmindspark <19kaushiks@gmail.com>
2018-08-18 11:57:31 -0700

    Working (not really) code

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
-PORT_DRIVELEFT = 1
-PORT_DRIVERIGHT = 2
-PORT_FLYWHEEL = 3
-PORT_INTAKE = 3
+#define PORT_DRIVELEFT 1
+#define PORT_DRIVERIGHT 2
+#define PORT_FLYWHEEL 3
+#define PORT_INTAKE 4
 
-HIGHFLAGPOWER = 95
-MIDDLEFLAGPOWER = 60
+#define HIGHFLAGPOWER 95
+#define MIDDLEFLAGPOWER 60
 
-OVERRIDETEMP = 0
+#define OVERRIDETEMP false
 
-void drive(){
+void drive(void* param){
     while (true) {
         int left = controller_get_analog(CONTROLLER_MASTER, ANALOG_LEFT_Y);
         int right = controller_get_analog(CONTROLLER_MASTER, ANALOG_RIGHT_Y);
@@ -21,21 +21,21 @@ void drive(){
   }
 }
 
-void flywheel(){
+void flywheel(void* param){
     while (true) {
-        if (E_CONTROLLER_DIGITAL_X){
+        if (E_CONTROLLER_DIGITAL_X == 1){
             motor_move_velocity(PORT_FLYWHEEL, HIGHFLAGPOWER);
         }
-        if (E_CONTROLLER_DIGITAL_A){
+        if (E_CONTROLLER_DIGITAL_A == 1){
             motor_move_velocity(PORT_FLYWHEEL, MIDDLEFLAGPOWER);
         }
-        if (E_CONTROLLER_DIGITAL_B || (motor_get_temperature(PORT_FLYWHEEL) > 45 && OVERRIDETEMP == 0)){
+        if (E_CONTROLLER_DIGITAL_B == 1 || (motor_get_temperature(PORT_FLYWHEEL) > 45 && !OVERRIDETEMP)){
             motor_move(PORT_FLYWHEEL, 0);
         }
     }
 }
 
-void intake(){
+void intake(void* param){
     while (true) {
         if (E_CONTROLLER_DIGITAL_L1){
             if (motor_get_power(PORT_INTAKE) != 127){
@@ -57,7 +57,7 @@ void intake(){
 }
 
 void opcontrol() {
-    task_t driveTask = task_create(drive, TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Drive Task")
-    task_t flywheelTask = task_create(flywheel, TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Flywheel Task")
-    task_t intakeTask = task_create(intake, TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Intake Task")
+    task_t driveTask = task_create(drive, "PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Drive Task");
+    task_t flywheelTask = task_create(flywheel, "PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Flywheel Task");
+    task_t intakeTask = task_create(intake, "PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Intake Task");
 }
